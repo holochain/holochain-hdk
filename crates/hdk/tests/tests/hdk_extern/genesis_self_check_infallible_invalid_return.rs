3610719ff5d02841ef7d40c1aa1f815c7f0505b9
Collins Muriuki <hello@collinsmuriuki.xyz>
2025-03-20 17:06:41 +0300

    chore: enable and move hdk extern tests (#4799)
    * Fix failing tests
    
    * Move tests
    
    * Remove cyclic dev dependency

--- crates/hdk/tests/tests/hdk_extern/genesis_self_check_infallible_invalid_return.rs
+++
@@ -1,8 +0,0 @@
-use hdk::prelude::*;
-
-#[hdk_extern(infallible)] 
-fn genesis_self_check() -> ExternResult<String> {
-    Ok("wrong return type".into())
-}
-
-fn main() {}
--- crates/hdk/tests/tests/hdk_extern/init_infallible_invalid_return.rs
+++
@@ -1,8 +0,0 @@
-use hdk::prelude::*;
-
-#[hdk_extern(infallible)] 
-fn init() -> String {
-    "wrong return type".into()
-}
-
-fn main() {}
